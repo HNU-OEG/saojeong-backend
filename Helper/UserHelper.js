def74c7579486c7e2704084b9cb0b19daf7b6dc3
Pete Lee <welikeheon@gmail.com>
2020-07-26 20:10:22 +0900

    익명 닉네임 생성 코드 추가

--- Helper/UserHelper.js
+++ Helper/UserHelper.js
@@ -1,5 +1,8 @@
 const pool = require('../config/db');
-
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs').promises
+const path = require('path')
 module.exports = {
     checkUserAvailablebyMemberID: async (member_id) => {
         try {
@@ -14,4 +17,16 @@ module.exports = {
             throw new Error("MemberID를 조회하는 중에 오류가 발생하였습니다.");
         }
     },
+    generateGuestNickname: async () => {
+
+        try {
+            const result = await fs.readFile(path.join(__dirname, 'dictionary.txt'), 'utf8');
+            var d = result.split("\n")
+            var item = d[Math.floor(Math.random() * d.length)];
+            nickname = "익명의 " + item
+            return nickname
+        } catch (e) {
+            console.error(e);
+        }
+    }
 }
