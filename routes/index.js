0e7a69573da83ca8d0d22dedfb7853022b93c2f7
Pete Lee <welikeheon@gmail.com>
2020-07-26 21:34:04 +0900

    새로운 익명 계정 추가(라우팅)

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,6 @@ const BoardHandler = require('./functions/Board/handler');
 const UserHandler = require('./functions/User/handler');
 
 
-router.post('/admin/api/customers.json', UserHandler.CreateNewGuestUser);
-
-
-router.get('/generate-user', UserHandler.CreatePhoneUser);
-router.get('/generate-session', UserHandler.CreatePhoneSession);
-router.get('/test-ref', UserHandler.CheckRelation);
+router.post('/admin/api/guest/customers.json', UserHandler.ClaimNewGuestUser);
 
 module.exports = router;
