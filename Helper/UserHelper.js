efebec74f6a5221ef01ae0e34ad0a48c0ecd5e2c
Pete Lee <welikeheon@gmail.com>
2020-07-26 16:07:39 +0900

    Member ID 사용 검증 function 추가

--- Helper/UserHelper.js
+++ Helper/UserHelper.js
@@ -1,9 +1,17 @@
+const pool = require('../config/db');
 
 module.exports = {
-    // CheckUsername: async (username) => {
-
-    // },
-    // CheckNickname: async () => {
-
-    // },
+    checkUserAvailablebyMemberID: async (member_id) => {
+        try {
+            let [result] = await pool.query('select member_id from users where member_id = ?', [member_id])
+            if (result.length == 0) {
+                return false;
+            } else {
+                return true;
+            }
+        } catch (err) {
+            console.log(err);
+            throw new Error("MemberID를 조회하는 중에 오류가 발생하였습니다.");
+        }
+    },
 }
