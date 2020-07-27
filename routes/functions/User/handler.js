2022ce62adb340388910ff748f1de356702f4020
Pete Lee <welikeheon@gmail.com>
2020-07-27 18:31:29 +0900

    새로운 사용자 생성 기능 및 사용자 수정 기능 추가

--- routes/functions/User/handler.js
+++ routes/functions/User/handler.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const faker = require('faker');
 const UserHelper = require('../../../Helper/UserHelper');
 const pool = require('../../../config/db');
+const jwt = require('jsonwebtoken');
+const passport = require('../../../config/passport')
 
 module.exports = {
     ClaimNewGuestUser: async (req, res, next) => {
@@ -24,14 +26,65 @@ module.exports = {
         try {
             const user = await pool.execute("insert into users (username, nickname, gender, type, enabled, last_updated_ip) values (?,?,?,?,?,?)", [data.username, data.nickname, data.gender, data.type, data.enabled, data.ipaddr]);
             const [result] = await pool.query("select member_id, username, nickname, gender, created_at from users where member_id=last_insert_id()")
-            return res.status(201).json(result[0])
+            console.log(result)
+            const token = jwt.sign(JSON.stringify(result[0]), process.env.JWT_SECRET);
+            return res.status(201).json({ result, token })
         } catch (err) {
             console.log(err)
             res.status(503).json({ "result": "error" });
             throw new Error("사용자 생성 중 오류 발생", err);
         }
     },
+    // CreateJWTToken: async (req, res, next) => {
+    //     passport.authenticate('jwt', { session: false }, (err, user) => {
+    //         if (err || !user) {
+    //             return res.status(400).json({
+    //                 message: 'Something is not right',
+    //                 user: user
+    //             });
+    //         }
+    //         req.login(user, { session: false }, (err) => {
+    //             if (err) {
+    //                 res.send(err);
+    //             }
+    //             // jwt.sign('token내용', 'JWT secretkey')
+    //             const token = jwt.sign(user.toJSON(), process.env.JWT_SECRET);
+    //             return res.json({ user, token });
+    //         });
+    //     });
+    // },
+    EditUserInfomation: async (req, res, next) => {
+        let member_id = req.params.member_id; // 파라미터의 사용자 id
+        let auth_member_id = req.user.member_id; // jwt의 사용자 id
 
+        if (!member_id == auth_member_id) {
+            return res.status(503).json({ "result": "error", "message": "인증되지 않은 사용자입니다." });
+        } else {
+            let nickname = req.body.nickname;
+            let userphoto = req.body.userphoto;
+            const ipaddr = req.header('x-forwarded-for') || req.connection.remoteAddress
+            var data = {
+                nickname: nickname,
+                userphoto: userphoto,
+                ip: ipaddr
+            }
+
+            try {
+                let user = await pool.execute("INSERT INTO users ( nickname, last_updated_id, last_updated_ip ) SELECT ?, ?, ? FROM users WHERE member_id = ?", [data.nickname, member_id, data.ip, member_id]);
+                const [result] = await pool.query("select member_id, username, nickname, gender, created_at from users where member_id=last_insert_id()")
+                console.log(user)
+                const token = jwt.sign(JSON.stringify(result[0]), process.env.JWT_SECRET);
+                console.log(token)
+                return res.json(201, { result, token })
+            } catch (err) {
+                console.log(err)
+                res.status(503).json({ "result": "error" });
+                throw new Error("사용자 정보 변경에 실패하였습니다.", err);
+            }
+
+        }
+
+    },
     // 폰은정....
     CreatePhoneUser: async (req, res, next) => {
         faker.locale = 'ko'
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,16 @@ const express = require('express');
 const router = express.Router();
 const BoardHandler = require('./functions/Board/handler');
 const UserHandler = require('./functions/User/handler');
+const passport = require('../config/passport')
 
+// router.get('/users', passport.authenticate('jwt', {session: false}), UserController.index);
+// router.post('/auth/tokens', UserHandler.CreateJWTToken);
 
 router.post('/admin/api/guest/customers.json', UserHandler.ClaimNewGuestUser);
+router.put('/admin/api/edit/customers/:member_id.json', passport.authenticate('jwt', { session: false }), UserHandler.EditUserInfomation);
+router.get("/hello", passport.authenticate('jwt', { session: false }), (req, res, next) => {
+    res.send("HELLO!!");
+})
+
 
 module.exports = router;
