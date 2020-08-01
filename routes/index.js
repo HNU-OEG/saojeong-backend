f28c35dbadc9a709ae443e36ff45355c08792b34
plzprayme <dbfpzk142@gmail.com>
2020-08-01 18:25:14 +0900

    board route file 분리

--- routes/index.js
+++ routes/index.js
@@ -31,16 +31,4 @@ router.get('/hello', passport.authenticate('jwt', { session: false }), (req, res
 router.get('/generate-user', UserHandler.CreatePhoneUser)
 router.get('/generate-session', UserHandler.CreatePhoneSession)
 
-
-// 게시판 관련
-router.post('/api/board', BoardHelper.CreateBoard)
-
-// 게시글 관련
-router.get('/api/board/:category/content', BoardHelper.GetAllBoardContentOrderByMethod)
-router.get('/api/board/:category/content/:documentId', BoardHelper.GetBoardContent)
-router.post('/api/board/:category/content', BoardHelper.CreateBoardContent)
-router.put('/api/board/:category/content/:documentId', BoardHelper.UpdateBoardContent)
-router.delete('/api/board/:category/content/:documentId', BoardHelper.DeleteBoardContent)
-router.patch('/api/board/:category/content/:documentId', BoardHelper.PatchBoardContentVoteOrBlame)
-
 module.exports = router
