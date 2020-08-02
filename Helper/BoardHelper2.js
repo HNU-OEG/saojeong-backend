c57c4df7cb4f958e9e8b967406f8f974eca791b4
Pete Lee <welikeheon@gmail.com>
2020-08-02 14:53:11 +0900

    Inspect Comment Author function 추가

--- Helper/BoardHelper2.js
+++ Helper/BoardHelper2.js
@@ -11,5 +11,17 @@ module.exports = {
     } catch {
       throw new Error('Board Content 의 상태를 조회하는데에 실패했습니다.')
     }
+  },
+  inspectCommentAuthor: async (comment_id) => {
+    try {
+      let [user] = await pool.query('select member_id from board_comments where comment_id = ?', [comment_id])
+      if (user.length == 1) {
+        return user[0].member_id
+      } else {
+        return false
+      }
+    } catch (err) {
+      throw new Error('Comment Author를 조회하지 못했습니다.')
+    }
   }
 }
