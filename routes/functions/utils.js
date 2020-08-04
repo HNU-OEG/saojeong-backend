8d7a4c88473ef8d958ef4b628b029a881d90b6ab
Pete Lee <welikeheon@gmail.com>
2020-08-04 23:34:24 +0900

    유틸리티 추가

--- routes/functions/utils.js
+++ routes/functions/utils.js
@@ -1,7 +1,16 @@
-const generateURLbyQueryString = (url, params) => {
-  let url = new URL(url)
-  let params = params
-  return Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+const { getLatestUpdateReferenceId } = require('../../Helper/UserHelper')
+const { separator } = require('faker/lib/locales/en')
+
+const generateURLbyQueryString = (base_url, parameters) => {
+  let url = new URL(base_url)
+  let params = parameters
+  Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+  return url
 }
 
+const getToday = (separator = '-') => {
+  const _date = new Date()
+  return [_date.getFullYear(), ('0' + (_date.getMonth() + 1)).slice(-2), ('0' + _date.getDate()).slice(-2)].join(separator)
+}
 module.exports.generateURLbyQueryString = generateURLbyQueryString
+module.exports.getToday = getToday
