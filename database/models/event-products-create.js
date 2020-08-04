30e440e506926b3a01f1463ce666ddd804afbbff
Pete Lee <welikeheon@gmail.com>
2020-08-04 23:33:44 +0900

    Priceset 모델 및 스키마 정의

--- database/models/event-products-create.js
+++ database/models/event-products-create.js
@@ -1,12 +1,19 @@
 const dynamoose = require('../dynamodb')
 const schema = require('../schemas/event-products-create')
 const Event = dynamoose.model('event-products-create', schema)
-
+const uuid = require('uuid')
 module.exports = {
   model: Event,
-  createPriceSet: async (priceset) => {
-    let newPriceSet = new Event(priceset)
+  createPriceSet: async (priceset, categoryCode, regDate) => {
+    var _priceset = {}
+    _priceset.id = await uuid.v1()
+    _priceset.categoryCode = categoryCode
+    _priceset.regDate = regDate
+    _priceset.kamisProductJSON = priceset
+    let newPriceSet = new Event(_priceset)
+    console.log(newPriceSet)
     return newPriceSet.save().then(data => {
+      console.log(data)
       return data
     })
   }
--- database/schemas/event-products-create.js
+++ database/schemas/event-products-create.js
@@ -12,11 +12,37 @@ module.exports = new Schema({
     required: true
   },
   regDate: {
-    type: Date,
-    require: true
+    type: 'String',
+    required: true
   },
   kamisProductJSON: {
-    type: [Object],
-    required: true
+    type: Array,
+    schema: [{
+      'type': Object,
+      'schema': {
+        id: { type: Number },
+        item_name: { type: String },
+        item_code: { type: String },
+        kind_name: { type: String },
+        kind_code: { type: String },
+        rank: { type: String },
+        rank_code: { type: String },
+        unit: { type: String },
+        day1: { type: String },
+        dpr1: { type: String },
+        day2: { type: String },
+        dpr2: { type: String },
+        day3: { type: String },
+        dpr3: { type: String },
+        day4: { type: String },
+        dpr4: { type: String },
+        day5: { type: String },
+        dpr5: { type: String },
+        day6: { type: String },
+        dpr6: { type: String },
+        day7: { type: String },
+        dpr7: { type: String },
+      }
+    }]
   }
 })
