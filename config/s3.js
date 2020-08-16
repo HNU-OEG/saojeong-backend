bc4d40bb10ef3c1b0b928ecbcc26baacd7eb5f66
plzprayme <dbfpzk142@gmail.com>
2020-08-16 14:35:48 +0900

    업로드 성공

--- config/s3.js
+++ config/s3.js
@@ -4,14 +4,13 @@ let multerS3 = require('multer-s3')
 
 let s3 = new aws.S3({
   secretAccessKey: process.env.AWS_ACCESS_KEYENV_VAR,
-  aceesKeyId: process.env.AWS_SECRET_KEYENV_VAR,
+  accesKeyId: process.env.AWS_SECRET_KEYENV_VAR,
 })
 
 var upload = multer({
   storage: multerS3({
     s3: s3,
     bucket: 'saojeong-images',
-    acl: 'public-read',
     key: function (req, file, cb) {
       cb(null, Date.now() + '.' + file.originalname.split('.').pop())
     }
