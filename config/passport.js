a192cac874d3d153525ac43bcc5a87f642ac54e5
Pete Lee <welikeheon@gmail.com>
2020-08-23 01:14:45 +0900

    Add facebook, kakao, google strategies

--- config/passport.js
+++ config/passport.js
@@ -3,10 +3,15 @@ const passportJWT = require('passport-jwt')
 const JWTStrategy = passportJWT.Strategy
 const ExtractJWT = passportJWT.ExtractJwt
 const FacebookStrategy = require('passport-facebook').Strategy
+const KakaoStrategy = require('passport-kakao-token').Strategy
+const GoogleTokenStrategy = require('passport-google-id-token')
+const FacebookTokenStrategy = require('passport-facebook-token')
+
 const randToken = require('rand-token')
 const UserHelper = require('../Helper/UserHelper')
 
 let pool = require('./db')
+const { claimNewSocialLoginJWTToken } = require('../Helper/UserHelper')
 require('dotenv').config()
 
 //JWT Strategy
@@ -36,13 +41,12 @@ passport.use(new JWTStrategy({
 }
 ))
 
-
-passport.use(new FacebookStrategy({
+// Facebook Strategy
+passport.use(new FacebookTokenStrategy({
   clientID: process.env.FACEBOOK_APP_ID,
   clientSecret: process.env.FACEBOOK_APP_SECRET,
-  callbackURL: 'http://localhost:3000/auth/facebook/callback',
-  profileFields: ['id', 'emails', 'name', 'displayName']
-}, async function (accessToken, refreshToken, profile, cb) {
+  fbGraphVersion: 'v7.0'
+}, async function (accessToken, refreshToken, profile, done) {
   var userdata = {
     member_id: randToken.suid(20),
     username: profile.id,
@@ -55,28 +59,84 @@ passport.use(new FacebookStrategy({
     refreshToken: refreshToken || null
   }
 
-  let [result] = await pool.query('select member_id, username, email, nickname, type from users where username = ?', profile.id)
+  let [result] = await pool.query('SELECT u.member_id, u.username, u.email, u.nickname, u.type, o.provider, o.oauth_version, o.access_token, o.refresh_token, o.expired_at, o.is_activated FROM users AS u, oauth_id AS o WHERE u.member_id = o.member_id AND o.is_activated=1 AND u.enabled=1 AND o.provider != "JWT" AND u.username = ?', profile.id)
+
 
   // 사용자 정보가 없을때
   if (result.length == 0) {
-    let user = await UserHelper.saveNewUserByOAuth(userdata.member_id, userdata.username, userdata.email, userdata.nickname, userdata.type)
-    let oauth = await UserHelper.saveNewOAuthInfo(userdata.member_id, userdata.provider, userdata.provider_version, userdata.accessToken, userdata.refreshToken)
-    return cb(null, userdata)
+    let user = await UserHelper.claimNewSocialLoginJWTToken(userdata.username, userdata.nickname, userdata.email, userdata.gender, userdata.ipaddr, userdata.provider, userdata.provider_version, userdata.accessToken, userdata.refreshToken, 1)
+    return done(null, user)
   } else {
     // 사용자 정보가 존재할 때
-    userdata = {
+    let jwtpayload_data = {
+      reference_id: result[0].id,
+      Provider: result[0].provider,
       member_id: result[0].member_id,
       username: result[0].username,
-      email: result[0].email,
       nickname: result[0].nickname,
-      type: result[0].type,
-      provider: 'Facebook',
-      provider_version: '7.0',
+      usertype: result[0].type
     }
-    return cb(null, userdata)
+    const refreshToken = await UserHelper.claimJWTRefreshTokenForcefully(jwtpayload_data.member_id)
+    const token = await UserHelper.claimJWTAccessToken(jwtpayload_data)
+    return done(null, { result, 'AccessToken': token, 'RefreshToken': refreshToken })
   }
-}
-))
+}))
+
+
+
+// Kakao Strategy
+passport.use(new KakaoStrategy({
+  clientID: '5d4b3b19341f825def8c0b5888858420',
+  clientSecret: 'ALc0wNfYeBAXBhnnac1GPc3fmyEaJMa7',
+  callbackURL: 'http://localhost:3000/auth/kakao/token'
+}, async function (accessToken, refreshToken, profile, done) {
+
+  console.log('profile :>> ', profile)
+
+  var userdata = {
+    member_id: randToken.suid(20),
+    username: profile.id,
+    email: profile._json.kakao_account.email || null,
+    nickname: profile.displayName,
+    gender: profile._json.kakao_account.gender || null,
+    type: 1,
+    provider: 'Kakao',
+    provider_version: 'v1',
+    accessToken: accessToken || null,
+    refreshToken: refreshToken || null
+  }
+
+  let [result] = await pool.query('SELECT u.member_id, u.username, u.email, u.nickname, u.type, o.provider, o.oauth_version, o.access_token, o.refresh_token, o.expired_at, o.is_activated FROM users AS u, oauth_id AS o WHERE u.member_id = o.member_id AND o.is_activated=1 AND u.enabled=1 AND o.provider != "JWT" AND u.username = ?', profile.id)
+
+
+  // 사용자 정보가 없을때
+  if (result.length == 0) {
+    let user = await UserHelper.claimNewSocialLoginJWTToken(userdata.username, userdata.nickname, userdata.email, userdata.gender, userdata.ipaddr, userdata.provider, userdata.provider_version, userdata.accessToken, userdata.refreshToken, 1)
+    return done(null, user)
+  } else {
+    // 사용자 정보가 존재할 때
+    let jwtpayload_data = {
+      reference_id: result[0].id,
+      Provider: result[0].provider,
+      member_id: result[0].member_id,
+      username: result[0].username,
+      nickname: result[0].nickname,
+      usertype: result[0].type
+    }
+    const refreshToken = await UserHelper.claimJWTRefreshTokenForcefully(jwtpayload_data.member_id)
+    const token = await UserHelper.claimJWTAccessToken(jwtpayload_data)
+    return done(null, { result, 'AccessToken': token, 'RefreshToken': refreshToken })
+  }
+}))
+
+passport.use(new GoogleTokenStrategy({
+  clientID: process.env.GOOGLE_CLIENT_ID,
+}, function (parsedToken, googleId, done) {
+  console.log('accessToken :>> ', parsedToken)
+  console.log('refreshToken :>>', googleId)
+  // console.log('profile :>> ', profile)
+
+}))
 
 
 passport.serializeUser(function (user, done) {
@@ -87,4 +147,4 @@ passport.deserializeUser(function (user, done) {
 })
 
 
-module.exports = passport
+module.exports = passport          
--- routes/auth.js
+++ routes/auth.js
@@ -4,9 +4,29 @@ const router = express.Router()
 const passport = require('../config/passport')
 const AuthHandler = require('./functions/Auth/handler')
 
-router.get('/facebook', passport.authenticate('facebook', { scope: 'email' }))
-router.get('/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/auth/failedLogin' }), AuthHandler.loginByFacebook)
+// Facebook
+// router.get('/facebook', passport.authenticate('facebook', { scope: 'email' }))
+// router.get('/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/auth/failedLogin' }), AuthHandler.loginByFacebook)
+// router.get('/failedLogin', AuthHandler.failedLogin)
+
+// Facebook
+// router.get('/facebook-token', passport.authenticate('facebook-token', { scope: 'email' }))
+router.get('/facebook/token', passport.authenticate('facebook-token', { failureRedirect: '/auth/failedLogin' }), AuthHandler.loginByFacebook)
 router.get('/failedLogin', AuthHandler.failedLogin)
+
+
+// Kakao
+router.get('/kakao', passport.authenticate('kakao-token'))
+router.get('/kakao/token', passport.authenticate('kakao-token', { failureRedirect: '/auth/failedLogin' }), AuthHandler.loginByFacebook)
+router.get('/kakao/callback', passport.authenticate('kakao-token', { failureRedirect: '/auth/failedLogin' }), AuthHandler.loginByFacebook)
+
+router.get('/failedLogin', AuthHandler.failedLogin)
+
+// Google
+router.get('/google', passport.authenticate('google-id-token'))
+router.get('/google/token', passport.authenticate('google-id-token', { failureRedirect: '/auth/failedLogin' }), AuthHandler.loginByFacebook)
+router.get('/failedLogin', AuthHandler.failedLogin)
+
 
 router.get('/logout', (req, res) => {
   req.logout()
