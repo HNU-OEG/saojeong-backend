49673bbb4ca0815c87f0706bc32df00370f8c7b8
Pete Lee <welikeheon@users.noreply.github.com>
2020-08-24 21:23:00 +0900

    Update after_install.sh

--- deployments/after_install.sh
+++ deployments/after_install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-/home/ubuntu/.nvm/versions/node/v10.15.3/bin/pm2 restart /home/saojeong/dist/app.js --watch
+/home/ubuntu/.nvm/versions/node/v10.15.3/bin/pm2 delete all
+/home/ubuntu/.nvm/versions/node/v10.15.3/bin/pm2 start /home/saojeong/dist/app.js --watch
