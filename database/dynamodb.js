c530bb527fe1a9ce2da2b69670fb838a8817e479
Pete Lee <welikeheon@gmail.com>
2020-08-26 02:10:48 +0900

    한번에 가져오는 JSON 개수 수정

--- database/dynamodb.js
+++ database/dynamodb.js
@@ -8,12 +8,15 @@ AWS.config.update({
 dynamoose.aws.sdk.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ENV_VAR,
   secretAccessKey: process.env.AWS_SECRET_KEY_ENV_VAR,
-  region: process.env.AWS_REGION_ENV_VAR
+  region: process.env.AWS_REGION_ENV_VAR,
+  maxRetries: 20,
 })
 
 dynamoose.model.defaults.set({
   create: true,
   prefix: process.env.DB_PREFIX,
   update: true,
+  throughput: 'ON_DEMAND'
 })
+
 module.exports = dynamoose
