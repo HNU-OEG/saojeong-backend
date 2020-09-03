fba2670d3d563372d0fb6b0c2b68b85341291e58
Pete Lee <welikeheon@users.noreply.github.com>
2020-09-03 10:44:03 +0900

    Update after_install.sh

--- deployments/after_install.sh
+++ deployments/after_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/saojeong/dist
-/home/ubuntu/.nvm/versions/node/v10.15.3/bin/pm2 delete all
-/home/ubuntu/.nvm/versions/node/v10.15.3/bin/pm2 start /home/saojeong/dist/server.js --env --watch
+/home/ubuntu/.nvm/versions/node/v10.15.3/bin/pm2 delete app
+/home/ubuntu/.nvm/versions/node/v10.15.3/bin/pm2 start /home/saojeong/dist/server.js --name app --env --watch
 rm after_install.sh appspec.yml before_install.sh package.json
